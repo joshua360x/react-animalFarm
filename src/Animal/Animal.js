d8a34f0c6fd1f3c014bf7c68c918fb5990ebbca2
joshua360x <jkw5499@psu.edu>
2022-01-24 21:04:41 -0500

    added styles from original data type

--- src/Animal/Animal.js
+++ src/Animal/Animal.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Animal.css';
 
-function Animal({ name, type, says }) {
-  return <div className='animalBox'>
+function Animal({ name, type, says, left, top }) {
+  return <div className='animalBox' style={{ left: { left }, top: { top } }}>
     {name}
     {says}
     <img className='animalImg' src={`./animals/${type}.svg`} alt={`${name}`} />
