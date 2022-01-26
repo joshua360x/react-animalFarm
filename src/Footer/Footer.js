f01827ec8e3e46159f1cb40559fba5b909a15e31
joshua360x <jkw5499@psu.edu>
2022-01-25 19:41:08 -0500

    styled the footer and animal listing to enable scroll

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -2,5 +2,5 @@ import React from 'react';
 import './Footer.css';
 
 export default function Footer({ email }) {
-  return <footer className='footer'>{email}</footer>;
+  return <footer className='footer'>Contact Me: {email}</footer>;
 }
